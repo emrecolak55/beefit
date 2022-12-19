8880bbcb2756e2b39aeaa2c186e1f209b5e6c22b
Emre Çolak <43623772+emrecolak55@users.noreply.github.com>
2022-12-20 00:11:28 +0300

    Login and Sign Up Pages updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
+import 'package:beefit/login.dart';
 import 'package:flutter/material.dart';
-import './login.dart';
 
-
-void main() {
-  runApp(MyApp());
+void main(List<String> args) {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage();
+      home: LoginPage(),
     );
   }
 }
